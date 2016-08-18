b08ecb9c962a5507fe71db5268338e3a77a9d170
Computer-Room <longtolin@foxmail.com>
2016-08-18 14:53:38 +0800

    Add genTRan(sta, end, t) founction

--- rodDetection.py
+++ rodDetection.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 def loadDataSet(filename):
     '''
     Extract the data from dataSet file
@@ -10,9 +10,43 @@ def loadDataSet(filename):
     dataArr = []
     for line in fr.readlines():
         line = line.strip().split(' ')
+        line = [float(line[i]) for i in range(len(line)) ]
         dataArr.append(line)
     dataArr = np.array(dataArr)
     lab = dataArr[:, 0]
     fea = dataArr[:,1:]
 
     return lab, fea
+
+def classifyResult(cla,label,  pred):
+    """
+
+    :rtype: object
+    """
+    print('rel\pre', end='  ')
+    for l in cla:
+        print(l, end='   ')
+    print('')
+    for l in cla:
+        ind = (label == l)
+        predl = pred[ind]
+        print(l, end='-----')
+        for j in cla:
+            print('%4d' % (predl == j).sum(), end='')
+        print('')
+
+
+def genTRan(sta, end, t):
+    '''
+    generate t random bootstrap numbers for start to end
+    产生t 个随机数（有放回） 从sta到end
+    :param sta: the start of the sequence
+    :param end: the end of the range(satr,end) sequence
+    :param t:  the number of results generated
+    :return: the random sequence ind (lsit)
+    '''
+    ind = []
+    for i in range(t):
+        ind.append(random.randint(sta, end)-1)
+    return ind
+
